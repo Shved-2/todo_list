080497455281276710628dac922ff6e38af63a43
Ilya Shved <ilia83ivanovich@gmail.com>
2020-12-21 16:22:14 +0500

    first commit

---
+++ src/components/List/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './List.css'
+
+
+const List = ({ items }) => {
+    return (
+        < ul className="list" >
+            {items.map(el => (
+                <li className={el.active ? 'active' : ''}>
+                    <i>
+                        {el.icon ? el.icon : <i className={`badge badge--${el.color}`}></i>}
+                    </i>
+                    <span>{el.name}</span>
+                </li>
+            ))
+            }
+
+        </ul >
+    )
+}
+export default List;
